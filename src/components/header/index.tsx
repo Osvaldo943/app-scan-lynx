7de950bb3c301e7bf47a7c06be67742403103b92
Osvaldo943 <osvaldodesousa9432@gmail.com>
2025-07-02 10:56:53 +0100

    style: improve header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,13 +3,18 @@ import './style.css';
 export function Header() {
   return (
     <view className="header-container">
-        <image
-          src="./assets/logo.png"
-          style={{ width: '100px', height: '60px' }}
-          tint-color='#ffffff'
-        />
-
-      <text className='text'>Scan</text>
+      <image
+        src="./assets/logo.png"
+        style={{
+          width: '100px',
+          height: '60px',
+          filter: 'brightness(0) invert(1)',
+        }}
+        tint-color="#ffffff"
+      />
+      <view className='btn-scan'>
+        <text className="text">Scan</text>
+      </view>
     </view>
   );
 }
