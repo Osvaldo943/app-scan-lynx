9da9a72343d0537dc2341dc5f1566d51899e4db8
Osvaldo943 <osvaldodesousa9432@gmail.com>
2025-07-02 10:22:48 +0100

    feat: update

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,16 @@
-import "./style.css"
+import './style.css';
 
-export function Header(){
-    return (
-        <view className="header-container">
-            <view>
-                <image src="./assets/logo.png" />
-            </view>
+export function Header() {
+  return (
+    <view className="header-container">
+      <view>
+        <image
+          src="./assets/logo.png"
+          style={{ width: '150px', height: '100px' }}
+        />
+      </view>
 
-            <text>Scan</text>
-        </view>
-    )
-} 
+      <text>Scan</text>
+    </view>
+  );
+}
