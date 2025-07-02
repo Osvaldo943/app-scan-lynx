396f3849d8cb04a3650e79a93b987449a91c3c8e
Osvaldo943 <osvaldodesousa9432@gmail.com>
2025-07-02 11:01:26 +0100

    feat: create logo component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,18 +1,11 @@
 import './style.css';
+import { Logo } from '../logo/index.js';
 
 export function Header() {
   return (
     <view className="header-container">
-      <image
-        src="./assets/logo.png"
-        style={{
-          width: '100px',
-          height: '60px',
-          filter: 'brightness(0) invert(1)',
-        }}
-        tint-color="#ffffff"
-      />
-      <view className='btn-scan'>
+      <Logo />
+      <view className="btn-scan">
         <text className="text">Scan</text>
       </view>
     </view>
---
+++ src/components/logo/index.tsx
@@ -0,0 +1,12 @@
+export function Logo() {
+  return (
+    <image
+      src="./assets/logo.png"
+      style={{
+        width: '100px',
+        height: '60px',
+        filter: 'brightness(0) invert(1)',
+      }}
+    />
+  );
+}
